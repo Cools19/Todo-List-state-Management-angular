14a75a8b34c5269b28636e5abb21bbe96d4e88aa
unknown <cogjava3106@iiht.tech>
2022-09-03 07:49:21 +0530

    added todo list

---
+++ src/app/reducers/todo.reducer.ts
@@ -0,0 +1,22 @@
+import { TodoActionType } from "../shared/enum/todo-action-types.enum";
+import { ActionParent } from "../action/todo.action";
+import { Todo } from "../models/Todo"; 
+
+
+const initialState: Todo[] = [
+    {title:""}
+   
+  
+];
+
+export function TodoReducer(state = initialState, action : ActionParent) {
+    switch(action.type) {
+        case TodoActionType.Add:
+            return[...state, action.payload]
+        case TodoActionType.Remove:
+            [...state.splice(action.payload,1)]
+            return [...state]
+        default:
+            return state;
+    }
+}
